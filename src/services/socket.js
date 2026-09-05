6125880db95380c36c7203a14b8c9534d23926d1
nishant226 <nishant73026@gmail.com>
2026-09-05 19:00:13 +0530

    Update socket URL to use production server IP

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+// Production ke liye EC2 IP ya environment variable use karein
+const SOCKET_URL = process.env.REACT_APP_BACKEND_URL || 'http://16.171.172.227:5000';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
