618f101e2788b3d6ac342bc569f0f7acf9ed6f03
nishant226 <nishant73026@gmail.com>
2026-09-05 19:04:21 +0530

    Fix socket URL by removing process.env reference for browser

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-// Production ke liye EC2 IP ya environment variable use karein
-const SOCKET_URL = process.env.REACT_APP_BACKEND_URL || 'http://16.171.172.227:5000';
+// Browser ke liye seedha production IP ya window location use karein
+const SOCKET_URL = 'http://16.171.172.227:5000';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
